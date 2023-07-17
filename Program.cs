5111f770cd850d52fb678d8bb9b0a052c2912586
demetbas <139221700+demetbas@users.noreply.github.com>
2023-07-17 19:37:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double sayi1, sayi2, sonuc;
+
+char islem;
+
+Console.WriteLine("Birinci Sayıyı Giriniz");
+
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("İkinci Sayıyı Giriniz");
+
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("İşlem Seçiniz");
+
+islem = Convert.ToChar(Console.ReadLine());
+
+switch (islem)
+{ 
+	case '+':
+	sonuc = sayi1 + sayi2;
+    Console.WriteLine("İşlem sonucu:" + sonuc);
+    break;
+
+	case '-':
+    sonuc = sayi1 - sayi2;
+    Console.WriteLine("İşlem sonucu:" + sonuc);
+    break;
+
+    case '*':
+    sonuc = sayi1 * sayi2;
+    Console.WriteLine("İşlem sonucu:" + sonuc);
+    break;
+
+    case '/':
+    sonuc = sayi1 / sayi2;
+    Console.WriteLine("İşlem sonucu:" + sonuc);
+    break;
+    
+   
+}
+
